4aff6ec745f4102b721d5cbfb42e00b0dee597c6
jpaark95 <yjpark@ucdavis.edu>
2021-04-18 16:11:55 -0700

    checksum.c working as intended

--- hw5/checksum.c
+++ hw5/checksum.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
+    int ch;
+    int checksum = 0;
+    
+    printf("Line: ");
+
+    while(1){
+        ch = getchar();
+        if (ch != '\n')
+            checksum += ch;
+        else
+            printf("Line: ");
+
+        if (checksum == '.')
+            break;
+
+        if (ch == '.'){
+            checksum = checksum % 64 + (int)' ';
+            printf("Checksum: %c\n", checksum);
+            checksum = 0;
+        }
+    }
 
     return 0;
 }
