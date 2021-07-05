322f783de88df3bae4cdcdefdd2d01440e14d291
jpaark95 <yjpark@ucdavis.edu>
2021-07-05 16:07:02 -0700

    names.c initial commit

---
+++ hw6/names.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void read_names(){
+}
+
+void write_names() {
+}
+
+void sort() {
+}
+
+int main(int argv, char** argc) {
+
+    return 0;
+}
