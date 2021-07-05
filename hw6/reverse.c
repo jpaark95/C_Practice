22800e488c9855b472ee152d800b673148dc0a1d
jpaark95 <yjpark@ucdavis.edu>
2021-07-04 23:07:10 -0700

    temp

--- hw6/reverse.c
+++ hw6/reverse.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#inlcude <string.h>
+#include <string.h>
 #include <ctype.h>
 
-int main() {
+#define MAX_CHAR 80
 
+int main() {
+    char inp[MAX_CHAR];
+    char tmp_cpy[MAX_CHAR];
+    char* word;
+    printf("Input: ");
+    fgets(inp, MAX_CHAR, stdin);
+    strcpy(tmp_cpy, inp);
+    printf("\n");
+    word = strtok(tmp_cpy, " \n");
+    while (word != NULL){
+        printf("word: %s\n", word);
+        word = strtok(NULL, " \n");
+    }
+    
     return 0;
 }
