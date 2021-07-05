8e946f57e72f627628bc92c1fded0ef5b35ba4ec
John <yjpark@ucdavis.edu>
2021-07-04 23:43:46 -0700

    reverse.c finished

--- hw6/reverse.c
+++ hw6/reverse.c
@@ -7,16 +7,29 @@
 int main() {
     char inp[MAX_CHAR];
     char tmp_cpy[MAX_CHAR];
-    char* word;
+    char* word[40];
+    int i = 0;
     printf("Input: ");
     fgets(inp, MAX_CHAR, stdin);
     strcpy(tmp_cpy, inp);
-    printf("\n");
-    word = strtok(tmp_cpy, " \n");
-    while (word != NULL){
-        printf("word: %s\n", word);
-        word = strtok(NULL, " \n");
+
+    word[0] = strtok(tmp_cpy, " \n");
+    i++;
+
+    while (1) { 
+        word[i] = strtok(NULL, " \n");     
+        if(word[i] == NULL)
+            break;
+
+        i++; 
     }
-    
+
+    i--;
+    printf("Reversed: ");
+    for(; i >= 0; i--){
+        printf("%s ", word[i]);
+    }
+
+    printf("\n");
     return 0;
 }
